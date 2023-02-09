9611eacdf4da91957fab2b3a05ff4d04fc474a02
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-02-09 18:25:45 +0100

    resorce classes created

--- src/api/AdminResource.java
+++ src/api/AdminResource.java
@@ -1,4 +1,36 @@
 package api;
 
+import model.Customer;
+import model.IRoom;
+
+import java.util.Collection;
+import java.util.List;
+
 public class AdminResource {
+
+    private static final AdminResource ADMIN_RESOURCE = new AdminResource();
+
+    private AdminResource(){
+
+    }
+
+    public Customer getCustomer(String email){
+        return null;
+    }
+
+    public void addRoom(List<IRoom> rooms){
+
+    }
+
+    public Collection<IRoom> getAllRooms(){
+        return null;
+    }
+
+    public Collection<Customer> getAllCustomers(){
+        return null;
+    }
+
+    public void displayAllReservations(){
+
+    }
 }
--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -1,4 +1,45 @@
 package api;
 
+import model.Customer;
+import model.IRoom;
+import model.Reservation;
+
+import java.util.Collection;
+import java.util.Date;
+
 public class HotelResource {
+
+    private static final HotelResource HOTEL_RESOURCE = new HotelResource();
+
+    private HotelResource(){
+
+    }
+
+    public Customer getCustomer(String email){
+        return null;
+    }
+
+    public void createACustomer(String email, String firstName, String lastName){
+
+    }
+
+    public IRoom getRoom(String roomNumber){
+        return null;
+    }
+
+    public Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date checkOutDate){
+        return null;
+    }
+
+    public Collection<Reservation> getCustomerReservation(String customerEmail){
+        return null;
+    }
+
+    public Collection<IRoom> findARoom(Date checkIn, Date checkOut){
+        return null;
+    }
+
+    public static HotelResource getHotelResource(){
+        return HOTEL_RESOURCE;
+    }
 }
