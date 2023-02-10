b41eb0b355f844a96f6367ad6d5344d9fd00953b
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-02-10 10:21:46 +0100

    customer repository done

---
+++ src/repositories/CustomerRepository.java
@@ -0,0 +1,39 @@
+package repositories;
+
+import model.Customer;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomerRepository {
+
+    private final Map<String, Customer> customers = new HashMap<>();
+
+    private static final CustomerRepository CUSTOMER_REPOSITORY = new CustomerRepository();
+
+    private CustomerRepository(){
+
+    }
+
+    public void createCustomer(String email, Customer customer){
+        customers.put(email, customer);
+    }
+
+    public Customer retrieveCustomer(String email){
+        return customers.get(email);
+    }
+
+    public Collection<Customer> retrieveAllCustomers(){
+        return new ArrayList<>(customers.values());
+    }
+
+    public void deleteCustomer(String email){
+        customers.remove(email);
+    }
+
+    public static CustomerRepository getCustomerRepository(){
+        return CUSTOMER_REPOSITORY;
+    }
+}
--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -1,28 +1,35 @@
 package service;
 
 import model.Customer;
+import repositories.CustomerRepository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class CustomerService {
 
     private static final CustomerService CUSTOMER_SERVICE = new CustomerService();
+    private final CustomerRepository customerRepository = CustomerRepository.getCustomerRepository();
 
     private CustomerService(){
     }
 
     public void addCustomer(String email, String firstName, String lastName){
-
+        Customer customer = new Customer(firstName, lastName, email);
+        customerRepository.createCustomer(email, customer);
     }
 
     public Customer getCustomer(String customerEmail){
-        return null;
+        return customerRepository.retrieveCustomer(customerEmail);
     }
 
     public Collection<Customer> getAllCustomers(){
-        return null;
+        return customerRepository.retrieveAllCustomers();
     }
 
+    public void deleteCustomerAccount(String email){
+        customerRepository.deleteCustomer(email);
+    }
     public static CustomerService getCustomerService(){
         return CUSTOMER_SERVICE;
     }
